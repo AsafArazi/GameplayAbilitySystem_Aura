aa2d1dac653e5401ef7f9762188c3dee275cfb07
Asaf Arazi <araziasaf@gmail.com>
2025-09-12 23:58:47 +0300

    Execute Local Gameplay Cues - 371

--- Source/Aura/Public/Actor/AuraProjectile.h
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -30,10 +30,10 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
-	virtual void Destroyed() override;
-
+	
 	UFUNCTION(BlueprintCallable)
-	void OnHit();
+	virtual void OnHit();
+	virtual void Destroyed() override;
 
 	UFUNCTION()
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
@@ -43,6 +43,9 @@ protected:
 
 	bool IsValidOverlap(AActor* OtherActor);
 	bool bHit = false;
+
+	UPROPERTY()
+	TObjectPtr<UAudioComponent> LoopingSoundComponent;
 private:
 
 	UPROPERTY(EditDefaultsOnly)
@@ -56,7 +59,4 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	TObjectPtr<USoundBase> LoopingSound;
-
-	UPROPERTY()
-	TObjectPtr<UAudioComponent> LoopingSoundComponent;
 };
